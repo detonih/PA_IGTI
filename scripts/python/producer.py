49e9abe6a69e5c631a47b7af46a1db4f32b573a7
Henrique Detoni <detonihenrique@gmail.com>
2021-11-24 21:55:17 -0300

    adiciona busca de informacao ininterrupta

--- scripts/python/producer.py
+++ scripts/python/producer.py
@@ -7,17 +7,20 @@ producer = KafkaProducer(
   bootstrap_servers='localhost:9092', #Kafka server
   value_serializer=lambda v: json.dumps(v).encode('utf-8') # Serialize json messages
 )
-
+# TODO: condição de se quer infos ou dividendos, splits... etc...
+# TODO: condição de qual empresa
 try:
-    change = yf.Ticker("GOOGL").info
-    print(type(change))
+    while True:
+        try:
+            change = yf.Ticker("GOOGL").info
+            
+        except ValueError:
+            pass
+        else:
+            right_now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
+            print(f"{right_now} :: Sending messages to Kafka Topic")
+            future = producer.send('finance', change)
+            result = future.get(timeout=60)
 except KeyboardInterrupt:
     print("process interrupted")
-except ValueError:
-    pass
-else:
-    right_now = datetime.now().strftime("%d/%m/%Y %H:%M:%S")
-    print(f"{right_now} :: Sending messages to Kafka Topic \n {change}")
-    future = producer.send('finance', change)
-    result = future.get(timeout=60)
 
